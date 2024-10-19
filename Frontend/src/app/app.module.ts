72effcd9bd58112318e2e6d3ddecc4f57ab6f346
Ronan Gomes <ronan_felipe@outlook.com>
2024-10-19 13:03:36 -0300

    Realiza estilização global e cria componente home

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SuppliersListComponent } from './suppliers/suppliers-list/suppliers-list.component';
+import { BtnHomeComponent } from './components/btn-home/btn-home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SuppliersListComponent,
+    BtnHomeComponent
   ],
   imports: [
     BrowserModule,
